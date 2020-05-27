4d7865f8584dbebc4c8304fbdb5a4a2719c99e83
Sydney <s1811552@s.tsukuba.ac.jp>
2020-05-27 17:19:36 +0900

    add new file

---
+++ flashcards/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from . import views
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path(r'^create/$', views.create_account, name='create_account'),
+    path(r'^login/$', LoginView.as_view(template_name='login.html'), name="login"),
+    path(r'^logout/$', LogoutView.as_view(template_name='index.html'), name='logout'),
+    path(r'^profile/$', views.Profile.as_view(), name='profile'),
+    
+]
